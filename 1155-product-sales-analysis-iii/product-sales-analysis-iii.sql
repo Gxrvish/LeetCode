c2c129ce8ab206db0447af84dcede3c902af9807
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-08-30 11:56:06 +0530

    Time: 1026 ms (58.84%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,3 +1,3 @@
-SELECT product_id, year AS first_year, quantity, price FROM sales
-WHERE (product_id, year) IN
-(SELECT product_id, MIN(year) AS year FROM sales GROUP BY product_id)
+select s1.product_id,s1.year as first_year,s1.quantity,s1.price 
+from Sales s1 where (s1.product_id,s1.year) in (select s2.product_id,min(s2.year) 
+from Sales s2 group by s2.product_id) 
