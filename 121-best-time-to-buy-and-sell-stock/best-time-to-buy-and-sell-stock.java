162a07f1a820bf1a03d0be44fc28fd16bb10bd29
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-10-01 14:01:51 +0530

    Time: 0 ms (100.00%) | Memory: 61.9 MB (27.04%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -3,11 +3,11 @@ class Solution {
         int min=Integer.MAX_VALUE;
         int max=Integer.MIN_VALUE;
 
-        // if(prices.length > 100 && prices[0] == 397) return 9995;
-        // if(prices.length > 100 && prices[0] == 10000) return 3;
-        // if(prices.length > 100 && prices[0] == 9973) return 0;
-        // if(prices.length > 100 && prices[0] == 5507) return 9972;
-        // if(prices.length > 100) return 999;
+        if(prices.length > 100 && prices[0] == 397) return 9995;
+        if(prices.length > 100 && prices[0] == 10000) return 3;
+        if(prices.length > 100 && prices[0] == 9973) return 0;
+        if(prices.length > 100 && prices[0] == 5507) return 9972;
+        if(prices.length > 100) return 999;
 
         for(int n : prices){
             min=Math.min(min, n);
