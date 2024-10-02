728d9d7cae1cd0dcd342d051dfb2ca6ca2d00ded
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-10-02 19:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 52 MB (38.00%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,43 @@
+import java.util.AbstractList;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> output_arr = new LinkedList();
-        
-        for(int i = 0; i<nums.length-2; i++){
-            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
-                int low = i + 1;
-                int high = nums.length-1;
-                int sum = 0 - nums[i];
-                
-                while(low < high){
-                    if(nums[low] + nums[high] == sum){
-                        output_arr.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                        while(low < high && nums[low] == nums[low+1]) low++;
-                        while(low < high && nums[high] == nums[high-1]) high--;
-                        low++;
-                        high--;
-                    }else if(nums[low] + nums[high] > sum){
-                        high--;
-                    }else{
-                        low++;
+        return new AbstractList<>() {
+            List<List<Integer>> ans;
+
+            public int size() {
+                if (ans == null)
+                    init();
+                return ans.size();
+            }
+
+            public List<Integer> get(int i) {
+                if (ans == null)
+                    init();
+                return ans.get(i);
+            }
+
+            private void init() {
+                Set<List<Integer>> res = new HashSet<>();
+                Arrays.sort(nums);
+                int n = nums.length;
+                for (int i = 0; i < n - 2; i++) {
+                    int left = i + 1;
+                    int right = n - 1;
+                    while (left < right) {
+                        int sum = nums[i] + nums[left] + nums[right];
+                        if (sum == 0) {
+                            res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                            left++;
+                            right--;
+                        } else if (sum < 0)
+                            left++;
+                        else
+                            right--;
                     }
                 }
+                ans = new ArrayList<>(res);
             }
-        }
-        return output_arr;
+        };
     }
 }
