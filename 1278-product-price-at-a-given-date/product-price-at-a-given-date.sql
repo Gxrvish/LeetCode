e3f65818feba2233f4aa713945b9c4d3c07b4ef3
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-10-02 12:44:35 +0530

    Time: 574 ms (30.16%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,24 +1,8 @@
-WITH
-  RankedProducts AS (
-    SELECT
-      product_id,
-      new_price,
-      RANK() OVER(
-        PARTITION BY product_id
-        ORDER BY change_date DESC
-      ) AS `rank`
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-  ),
-  ProductToLatestPrice AS (
-    SELECT product_id, new_price
-    FROM RankedProducts
-    WHERE `rank` = 1
-  )
-SELECT
-  Products.product_id,
-  IFNULL(ProductToLatestPrice.new_price, 10) AS price
+# Write your MySQL query statement below
+SELECT product_id, FIRST_VALUE(new_price) OVER(PARTITION BY product_id ORDER BY change_date DESC) AS price
 FROM Products
-LEFT JOIN ProductToLatestPrice
-  USING (product_id)
-GROUP BY 1;
+WHERE change_date <= '2019-08-16'
+UNION
+SELECT DISTINCT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (SELECT product_id FROM Products WHERE change_date <= '2019-08-16')
