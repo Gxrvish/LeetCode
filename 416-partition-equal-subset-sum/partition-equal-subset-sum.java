a1554035531faaa75762b60aa85b22f47fc8c603
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-12-18 12:37:53 +0530

    Time: 26 ms (82.60%) | Memory: 42 MB (88.42%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        // If total sum is odd, it cannot be partitioned into two equal subsets
+        if (totalSum % 2 != 0) return false;
+        
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; // A sum of 0 is always possible
+        
+        // Process each number in nums
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+}
