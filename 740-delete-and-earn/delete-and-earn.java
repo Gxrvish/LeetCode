b824a255f1c0a148d85baff9e28ab6fa47a608ea
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-12-30 16:21:52 +0530

    Time: 2 ms (99.82%) | Memory: 44.4 MB (75.58%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,20 +1,23 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        // if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        int max = 0;
+        for (int n : nums) {
+            if (n > max)
+                max = n;
+        }
 
-        int[] points = new int[10001];
-        Arrays.sort(nums);
-        for(int i = 0; i<nums.length; i++) {
-            points[nums[i]] += nums[i];
+        int sums[] = new int[max + 1];
+
+        for (int n : nums) {
+            sums[n] += n;
         }
-        
-        int[] dp = new int[10001];
-        dp[0] = points[0];
-        dp[1] = points[1];
-        for(int i=2; i<=10000; i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2] + points[i]);
+
+        int oneStepOut = 0, twoStepOut = 0;
+        for (int i = max; i >= 0; --i) {
+            int maxValue = Math.max(oneStepOut, sums[i] + twoStepOut);
+            twoStepOut = oneStepOut;
+            oneStepOut = maxValue;
         }
-        return dp[10000];
+        return oneStepOut;
     }
 }
