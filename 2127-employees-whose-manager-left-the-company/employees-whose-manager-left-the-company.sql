65cdd02f3791fdb121080c49248b2d2e9f2c6c89
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2024-12-31 16:19:50 +0530

    Time: 697 ms (12.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,10 @@
+SELECT employee_id
+FROM employees e1
+WHERE salary < 30000
+  AND manager_id IS NOT NULL
+  AND NOT EXISTS (
+      SELECT 1
+      FROM employees e2
+      WHERE e2.employee_id = e1.manager_id
+  )
+ORDER BY employee_id
