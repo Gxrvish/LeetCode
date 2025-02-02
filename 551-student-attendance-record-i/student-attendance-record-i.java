ca25db029258d6348ff4bbeca5e8c39e5db0b81d
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-02-02 13:21:38 +0530

    Time: 1 ms (32.94%) | Memory: 41.5 MB (81.21%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int absent = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == 'A') {
+                absent++;
+            }
+        }
+        if(absent > 1) return false;
+        for(int i=1; i<s.length()-1; i++) {
+            if(s.charAt(i) == 'L' && s.charAt(i-1) == 'L' && s.charAt(i+1) == 'L') return false;
+        }
+        return true;
+    }
+}
