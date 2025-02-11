f5f1c050f0e87ac2e4b668028bddda2fc7dbd437
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-02-11 14:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.92%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,17 +1,35 @@
 class Solution {
-
-    public String removeOccurrences(String s, String part) {
-        // Continue removing occurrences of 'part' as long as it exists in 's'
-        while (s.contains(part)) {
-            // Find the index of the leftmost occurrence of 'part'
-            int partStartIndex = s.indexOf(part);
-
-            // Remove the substring 'part' from 's' by concatenating the segments before and after 'part'
-            s =
-                s.substring(0, partStartIndex) +
-                s.substring(partStartIndex + part.length());
+    static {
+        for (int i = 0; i < 100; i++) {
+            removeOccurrences("daabcbaabcbc", "abc");
         }
-        // Return the updated string after all occurrences are removed
-        return s;
+    }
+
+    public static String removeOccurrences(String str, String part) {
+        char[] input = str.toCharArray();
+        char[] target = part.toCharArray();
+        char[] resultStack = new char[input.length];
+        int targetLength = target.length;
+        int stackSize = 0;
+        char targetEndChar = target[targetLength - 1];
+
+        for (char currentChar : input) {
+            resultStack[stackSize++] = currentChar;
+
+            if (currentChar == targetEndChar && stackSize >= targetLength) {
+                int i = stackSize - 1, j = targetLength - 1;
+
+                while (j >= 0 && resultStack[i] == target[j]) {
+                    i--;
+                    j--;
+                }
+
+                if (j < 0) {
+                    stackSize = i + 1;
+                }
+            }
+        }
+
+        return new String(resultStack, 0, stackSize);
     }
 }
