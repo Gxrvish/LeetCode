bcbf0e9551751a5449b16594dd557be48a9f1511
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-04-05 16:29:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (11.35%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,40 +2,40 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
-        // 'used' array keeps track of which numbers are already included in the current permutation
-        boolean[] used = new boolean[nums.length];
-        Arrays.fill(used, false); // initially no number is used
-
-        // Start backtracking with an empty permutation
-        backtrack(used, nums, result, new ArrayList<>(), 0);
+        // Start backtracking from index 0
+        backtrack(nums, 0, result);
         return result;
     }
 
-    // Backtracking function to build permutations
-    private void backtrack(boolean[] used, int[] nums, List<List<Integer>> result, List<Integer> currPermu, int start) {
-        // Base case: if the current permutation has reached full length
-        if (currPermu.size() == nums.length) {
-            // Add a copy of the current permutation to the result list
-            // Important: use 'new ArrayList<>()' to copy because 'currPermu' will be modified further
-            result.add(new ArrayList<>(currPermu));
+    // Backtracking by fixing one index at a time using swapping
+    private void backtrack(int[] nums, int index, List<List<Integer>> result) {
+        // Base case: if index has reached the end, store the current permutation
+        if (index == nums.length) {
+            List<Integer> currPermu = new ArrayList<>();
+            for (int num : nums) {
+                currPermu.add(num); // Copy array into list
+            }
+            result.add(currPermu);
             return;
         }
 
-        // Explore all possible numbers to place at current position
-        for (int i = 0; i < nums.length; i++) {
-            // Only consider numbers that haven’t been used in the current path
-            if (!used[i]) {
-                // Choose: add the number to current permutation and mark it as used
-                currPermu.add(nums[i]);
-                used[i] = true;
+        // Try placing every number at the current index
+        for (int i = index; i < nums.length; i++) {
+            // Swap nums[i] to the current index position
+            swap(nums, index, i);
 
-                // Recurse: build the next level of the permutation
-                backtrack(used, nums, result, currPermu, i + 1);
+            // Recurse to fix the next index
+            backtrack(nums, index + 1, result);
 
-                // Backtrack: remove the number and mark it as unused for other possibilities
-                currPermu.remove(currPermu.size() - 1);
-                used[i] = false;
-            }
+            // Backtrack: undo the swap to restore original state
+            swap(nums, index, i);
         }
+    }
+
+    // Swap helper function
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
