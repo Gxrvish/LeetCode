8293dd57a98972f8d685ff65b8c265bf27ca7a8f
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-05-07 21:38:47 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (39.89%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
-        for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 0)
-                dp[i][0] = dp[i - 1][0];
-        }
-        for (int j = 1; j < n; j++) {
-            if (obstacleGrid[0][j] == 0)
-                dp[0][j] = dp[0][j - 1];
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 0)
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = (grid[0][0] == 1) ? 0 : 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[j] = 0; // Obstacle blocks the path
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1]; // dp[j] is from top, dp[j-1] from left
+                }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
