e3701a61fa384a7f4c3a1ca10563626b985e8e70
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-05-10 00:23:13 +0530

    Time: 4 ms (44.43%) | Memory: 44.4 MB (44.33%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,16 +2,14 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int l = triangle.size();
         int[][] dp = new int[l][l];
-        for(int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        for(int i = 0; i < l; i++) {
+            dp[l-1][i] = triangle.get(l-1).get(i);
         }
-        return pickNotPick(triangle, 0, 0, dp); 
-    }
-    public int pickNotPick(List<List<Integer>> t, int i, int j, int[][] dp) {
-        if(i == t.size() - 1) return t.get(i).get(j);
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = pickNotPick(t, i + 1, j, dp);
-        int diag = pickNotPick(t, i + 1, j + 1, dp);
-        return dp[i][j] = t.get(i).get(j) + Math.min(down, diag);
+        for(int i = l-2; i>=0; i--) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 }
