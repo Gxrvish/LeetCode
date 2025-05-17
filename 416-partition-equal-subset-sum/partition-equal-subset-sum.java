2c4e8e98c11644b2ae18590b1560485d1ed2217b
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-05-17 15:31:20 +0530

    Time: 104 ms (11.94%) | Memory: 57.7 MB (14.85%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        
-        // If total sum is odd, it cannot be partitioned into two equal subsets
-        if (totalSum % 2 != 0) return false;
-        
-        int target = totalSum / 2;
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true; // A sum of 0 is always possible
-        
-        // Process each number in nums
-        for (int num : nums) {
-            for (int j = target; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
-        }
-        
-        return dp[target];
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        Boolean[][] dp = new Boolean[nums.length][target + 1];
+
+        return isPartition(0, target, nums, dp);
+    }
+
+    public boolean isPartition(int i, int sum, int[] nums, Boolean[][] dp) {
+        if (sum == 0) return true;
+        if (i == nums.length || sum < 0) return false;
+
+        if (dp[i][sum] != null) return dp[i][sum];
+
+        boolean pick = isPartition(i + 1, sum - nums[i], nums, dp);
+        boolean notPick = isPartition(i + 1, sum, nums, dp);
+
+        dp[i][sum] = pick || notPick;
+        return dp[i][sum];
     }
 }
