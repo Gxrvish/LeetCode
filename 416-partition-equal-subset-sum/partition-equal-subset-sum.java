99f38458c382eb63922a9bc17a0cb1ae7f9cdb09
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-05-20 18:18:01 +0530

    Time: 28 ms (61.59%) | Memory: 42 MB (86.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -3,27 +3,17 @@ class Solution {
         int sum = 0;
         for (int num : nums) sum += num;
         if (sum % 2 != 0) return false;
+        
         int target = sum / 2;
-        int n = nums.length;
-        boolean[][] dp = new boolean[n][target + 1];
-        // Base case: Any index can make sum 0 (by picking nothing)
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-        }
-        // Base case: First element alone can make nums[0] if it's within target
-        if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= target; j++) {
-                boolean notPick = dp[i - 1][j];
-                boolean pick = false;
-                if (j >= nums[i]) {
-                    pick = dp[i - 1][j - nums[i]];
-                }
-                dp[i][j] = pick || notPick;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-        return dp[n - 1][target];
+
+        return dp[target];
     }
 }
