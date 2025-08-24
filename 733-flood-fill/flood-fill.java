4f920cb6ce1652785ed08a4b4ae11dafb8918549
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-08-24 13:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (32.55%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,42 +1,26 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int row = image.length;
-        int column = image[0].length;
-        boolean[][] visited = new boolean[row][column];
-        int startingColor = image[sr][sc];
-        bfs(image, visited, color, startingColor, sr, sc);
-        return image;
-    }
-    public void bfs(int[][] grid, boolean[][] visited, int color, int startingColor, int i, int j) {
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        visited[i][j] = true;
-        grid[i][j] = color;
-
-        while(!q.isEmpty()) {
-            int[] cell = q.poll();
-            int r = cell[0], c = cell[1];
-
-            for(int[] d : dirs) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if (isAccessible(nr, nc, grid.length, grid[0].length) 
-                    && !visited[nr][nc] 
-                    && grid[nr][nc] == startingColor) {
-                    q.add(new int[]{nr, nc});
-                    visited[nr][nc] = true;
-                    grid[nr][nc] = color;
-                }
-            }
+        int colorCheck = image[sr][sc];
+        if(color == colorCheck){
+            return image;
+        } else {
+            fill(image, sr, sc, color, image[sr][sc]);
+            return image;
         }
     }
 
-    public boolean isAccessible(int nr, int nc, int row, int column) {
-        if(nr >= 0 && nr < row && nc >= 0 && nc < column) {
-            return true;
+    public void fill(int[][] image, int i, int j, int color, int prev){
+        if(i<0 || j<0 || i>image.length-1 || j>image[0].length-1){
+            return;
         }
-        return false;
+        if(image[i][j] != prev){
+            return;
+        } else {
+            image[i][j] = color;
+            fill(image, i-1, j, color, prev);
+            fill(image, i+1, j, color, prev);
+            fill(image, i, j-1, color, prev);
+            fill(image, i, j+1, color, prev);
+        }
     }
 }
