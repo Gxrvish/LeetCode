3a4c7a027ba4fb7246e96316963f9e9ed275e97f
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-11-16 12:12:50 +0530

    Time: 2 ms (16.03%) | Memory: 47.6 MB (8.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,20 +1,32 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[][] matrixCopy = new int[matrix.length][];
-        for (int i = 0; i < matrix.length; i++) {
-            matrixCopy[i] = matrix[i].clone();
-        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int index = 0;
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (matrixCopy[i][j] == 0) {
-                    for (int k = 0; k < matrix.length; k++) {
-                        matrix[k][j] = 0;
-                    }
-                    for (int l = 0; l < matrix[0].length; l++) {
-                        matrix[i][l] = 0;
-                    }
+                if (matrix[i][j] == 0) {
+                    // Ensure a new list for this index, then add both coords
+                    var coords = map.computeIfAbsent(index, _ -> new ArrayList<>());
+                    coords.add(i);
+                    coords.add(j);
+
+                    index++; // increment only once per zero cell
                 }
+            }
+        }       
+
+        for (var entry : map.entrySet()) {
+            // int index = entry.getKey();
+            List<Integer> coords = entry.getValue();
+
+            int row = coords.get(0);
+            int col = coords.get(1);
+            for (int k = 0; k < matrix.length; k++) {
+                matrix[k][col] = 0;
+            }
+            for (int l = 0; l < matrix[0].length; l++) {
+                matrix[row][l] = 0;
             }
         }
     }
