82789d2d72f4cd294f01cbaec1259f83d64bc5c2
Garvish Panchal <85312938+Gxrvish@users.noreply.github.com>
2025-11-17 22:46:11 +0530

    Time: 1 ms (65.71%) | Memory: 47.4 MB (9.34%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,32 +1,51 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int index = 0;
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    // Ensure a new list for this index, then add both coords
-                    var coords = map.computeIfAbsent(index, _ -> new ArrayList<>());
-                    coords.add(i);
-                    coords.add(j);
-
-                    index++; // increment only once per zero cell
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
-        }       
-
-        for (var entry : map.entrySet()) {
-            // int index = entry.getKey();
-            List<Integer> coords = entry.getValue();
-
-            int row = coords.get(0);
-            int col = coords.get(1);
-            for (int k = 0; k < matrix.length; k++) {
-                matrix[k][col] = 0;
+        }
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
             }
-            for (int l = 0; l < matrix[0].length; l++) {
-                matrix[row][l] = 0;
+        }
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
